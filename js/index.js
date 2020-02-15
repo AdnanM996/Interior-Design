7d2806dde281196d2225cd675e41de999cdc0295
adnan musinovic <musinovicadnan@gmail.com>
2020-02-15 18:04:04 +0100

    interior design

---
+++ js/index.js
@@ -0,0 +1,27 @@
+let slideIndex = 0;
+showSlides();
+let dots = document.querySelectorAll('.dot');
+
+function showSlides() {
+
+    let slides = document.querySelectorAll('.slides');
+    let dots = document.querySelectorAll('.dot');
+
+    for(let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+
+    slideIndex++;
+
+    if(slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    for(let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(' active', '');
+    }
+
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].className += ' active';
+    setTimeout(showSlides, 5000);
+}
